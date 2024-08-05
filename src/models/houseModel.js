5a73d009a021291561c12e860bcecb7deeb74ce5
Ahmed Mukhtar <mukhtar1100@hotmail.com>
2024-08-05 21:52:32 +0200

    ok

--- src/models/houseModel.js
+++ src/models/houseModel.js
@@ -7,16 +7,26 @@ import mongoose from "mongoose";
 const houseSchema = new mongoose.Schema({
     houseType: {
         type: String,
-        enum: ['Fillo', 'Dabaq', 'Jiingad', 'Baacweeyne', 'Modul', 'Hotel'],
+        enum:
+            ['Fillo', 'Guri Dabaq ah', 'Guri Jiingad', 'Baacweeyne',
+            'Modul', 'Hotel','Guri firaashan'
+            ],
         required: true,
-        message: 'Vänligen välj en huskategori.',
+        message: 'Dooro nuuca guriga aa dooneeysid, walaal.',
     },
+
+    //gör om det till och fixa controll class för att kunna hantera detta dymaniskt
     houseTransactions: {
         type: String,
-        enum: ['Kiro', 'Iibin', 'Badal', 'Iibsasho'],
+        enum: ['Kiro', 'Iibin', 'Iibsasho', 'Baddal'],
         required: true,
         message: 'Husstatus är obligatoriskt.',
     },
+    houseStairs: {
+        type: String,
+        enum: ['Dabaq 1 aad ','Dabaq 2 aad','Dabaq 3 aad','Dabaq 4aad '],
+    },
+
     description: { type: String },
     bathrooms: {
         type: Number,
@@ -32,6 +42,10 @@ const houseSchema = new mongoose.Schema({
         type: Number,
         required: true,
         message: 'Ballaca dhulka waa lama huraan'
+    },
+    houseKitchen:{
+        type: String,
+        enum:['Haa','Maya']
     },
     houseHeight: {
         type: Number,
@@ -53,6 +67,11 @@ const houseSchema = new mongoose.Schema({
         required: true,
         message: 'Antal rum är obligatoriskt.',
     },
+
+    roomType: { type: String, enum: ['single room', 'double room'], default: 'single room' },
+    privateBathroom: { type: Boolean, enum:['Yes','No'], default: false },
+    // Common attributes
+    available: { type: Boolean, default: true },
     houseWifi: {
         type: String,
         enum: ['Haa', 'Maya'],
@@ -79,7 +98,9 @@ const houseSchema = new mongoose.Schema({
         required: true,
         message: 'Antal toaletter är obligatoriskt.',
     },
-/*    images: [imageSchema],*/
+    toiletType: { type: String, enum: ['private', 'public'], default: 'public' },
+
+    /*    images: [imageSchema],*/
     houseParking: {
         type: String,
         enum: ['Garage', 'Street', 'None'],
